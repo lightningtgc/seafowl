1054673d8666f39171d8380f41a085f4fbccf3a3
gctang <lightningtgc@gmail.com>
2015-05-30 19:21:32 +0800

    Update compile.js

--- src/compile/compile.js
+++ src/compile/compile.js
@@ -1,1 +1,18 @@
+walking(node, callback)  => {
+  if (node.nodeType === 1 || node.nodeType === 3) {
+    var returnValue = callback(node);
+    if (returnValue === false) {
+      return;
+    }
+  }
+
+  if (node.nodeType === 1) {
+    var current = node.firstChild;
+    while (current) {
+      walking(current, callback);
+      current = current.nextSibling;
+    }
+  }
+}
+
 
